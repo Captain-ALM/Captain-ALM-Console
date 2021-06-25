da4f0c5ceb060ddcda47cdf19e04e8a938774c8c
Captain ALM <alm00000@outlook.com>
2021-06-25 16:26:46 +0100

    Add netfw Solution with the initial library project.

---
+++ netfw/Captain-ALM-Console/Captain-ALM-Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Captain-ALM-Console")]
+[assembly: AssemblyDescription("Captain ALM Console Library and API")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Captain ALM")]
+[assembly: AssemblyProduct("Captain-ALM-Console")]
+[assembly: AssemblyCopyright("Copyright © Captain ALM 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d872eb88-04b4-43c4-aeca-7740704b7239")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]

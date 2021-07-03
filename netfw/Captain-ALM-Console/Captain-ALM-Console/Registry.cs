790f48aa196441b0a8f6cbee2fe1b18318a1f8b4
Captain ALM <alm00000@outlook.com>
2021-07-03 21:19:44 +0100

    Finish the registry for ICommand and ISyntax.

--- netfw/Captain-ALM-Console/Captain-ALM-Console/Registry.cs
+++ netfw/Captain-ALM-Console/Captain-ALM-Console/Registry.cs
@@ -9,8 +9,10 @@ namespace captainalm.calmcmd
     /// </summary>
     public sealed class Registry
     {
-        internal static List<ICommand> _commands = new List<ICommand>();
-        internal static List<ISyntax> _syntaxes = new List<ISyntax>();
+        private static List<ICommand> _commands = new List<ICommand>();
+        private static List<ISyntax> _syntaxes = new List<ISyntax>();
+        private static object slockcommands = new object();
+        private static object slocksyntaxes = new object();
 
         /// <summary>
         /// Adds a command to the Console
@@ -21,10 +23,12 @@ namespace captainalm.calmcmd
         public static void registerCommand(ICommand commandIn)
         {
             if (Object.ReferenceEquals(commandIn, null)) throw new ArgumentNullException("commandIn");
-            if (_commands.Exists(delegate(ICommand x) { return NameComparer(x, commandIn); })) throw new CaptainALMConsoleException("Duplicated Command", new ArgumentException("commandIn"));
-            _commands.Add(commandIn);
+            lock (slockcommands)
+            {
+                if (_commands.Exists(delegate(ICommand x) { return NameComparer(x, commandIn); })) throw new CaptainALMConsoleException("Duplicated Command", new ArgumentException("commandIn"));
+                _commands.Add(commandIn);
+            }
         }
-
         /// <summary>
         /// Adds a syntax to the Console
         /// </summary>
@@ -34,8 +38,63 @@ namespace captainalm.calmcmd
         public static void registerSyntax(ISyntax syntaxIn)
         {
             if (Object.ReferenceEquals(syntaxIn, null)) throw new ArgumentNullException("syntaxIn");
-            if (_syntaxes.Exists(delegate(ISyntax x) { return NameComparer(x, syntaxIn); })) throw new CaptainALMConsoleException("Duplicated Syntax", new ArgumentException("syntaxIn"));
-            _syntaxes.Add(syntaxIn);
+            lock (slocksyntaxes)
+            {
+                if (_syntaxes.Exists(delegate(ISyntax x) { return NameComparer(x, syntaxIn); })) throw new CaptainALMConsoleException("Duplicated Syntax", new ArgumentException("syntaxIn"));
+                _syntaxes.Add(syntaxIn);
+            }
+        }
+        /// <summary>
+        /// Removes a command from the console
+        /// </summary>
+        /// <param name="commandIn">The command to remove</param>
+        /// <exception cref="System.ArgumentNullException">The parameter is null</exception>
+        /// <exception cref="captainalm.calmcmd.CaptainALMConsoleException">The command does not exists</exception>
+        public static void removeCommand(ICommand commandIn)
+        {
+            if (Object.ReferenceEquals(commandIn, null)) throw new ArgumentNullException("commandIn");
+            lock (slockcommands)
+            {
+                if (!_commands.Exists(delegate(ICommand x) { return NameComparer(x, commandIn); })) throw new CaptainALMConsoleException("No Command", new ArgumentException("commandIn"));
+                _commands.RemoveAll(delegate(ICommand x) { return NameComparer(x, commandIn); });
+            }
+        }
+        /// <summary>
+        /// Removes a syntax from the console
+        /// </summary>
+        /// <param name="syntaxIn">The syntax to remove</param>
+        /// <exception cref="System.ArgumentNullException">The parameter is null</exception>
+        /// <exception cref="captainalm.calmcmd.CaptainALMConsoleException">The syntax does not exists</exception>
+        public static void removeSyntax(ISyntax syntaxIn)
+        {
+            if (Object.ReferenceEquals(syntaxIn, null)) throw new ArgumentNullException("syntaxIn");
+            lock (slocksyntaxes)
+            {
+                if (!_syntaxes.Exists(delegate(ISyntax x) { return NameComparer(x, syntaxIn); })) throw new CaptainALMConsoleException("No Syntax", new ArgumentException("syntaxIn"));
+                _syntaxes.RemoveAll(delegate(ISyntax x) { return NameComparer(x, syntaxIn); });
+            }
+        }
+        /// <summary>
+        /// Returns all the currently registered commands
+        /// </summary>
+        /// <returns>An array of registered commands</returns>
+        public static ICommand[] getCommands()
+        {
+            lock (slockcommands)
+            {
+                return _commands.ToArray();
+            }
+        }
+        /// <summary>
+        /// Returns all the currently registered syntaxes
+        /// </summary>
+        /// <returns>An array of registered syntaxes</returns>
+        public static ISyntax[] getSyntaxes()
+        {
+            lock (slocksyntaxes)
+            {
+                return _syntaxes.ToArray();
+            }
         }
 
         private static bool NameComparer(IName x, IName y)
